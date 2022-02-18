559fbf6c3cc5c0dbbe45208a9f264deb7e19249a
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-18 18:30:02 +0200

    final1

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ import sys
 import random
 
 
-def permutations(s):
-    if len(s) <= 1:
-        return [s]
+def permutations(sample):
+    if len(sample) <= 1:
+        yield sample
     else:
-        perms = []
-        for e in permutations(s[:-1]):
-            for i in range(len(e)+1):
-                perms.append(e[:i] + s[-1] + e[i:])
-        return perms
+        for elem in permutations(sample[1:]):
+            for i in range(len(sample)+1):
+                yield elem[:i] + sample[0:1] + elem[i:]
+        return sample
 
 
-if len(sys.argv) != 2:
-    sys.stderr.write('Exactly one argument is required \n')
-    sys.exit(1)
+# if len(sys.argv) != 2:
+#     sys.stderr.write('Exactly one argument is required \n')
+#     sys.exit(1)
 
 
 # getting the word for permutations
 x = input("word?\n")
 
 # saving the result to a variable, making operations with it possible
-z = permutations(x)
+z = list(permutations(x))
 
 # printing randomly chosen 20 items from the list
 print(random.sample(z, 20))
